86b7f709838ac3358ba3c0f932212725a34daf80
“jameela-khan” <jameelakhan52@gmail.com>
2023-01-10 13:20:37 +0530

    made error files

--- backened/controller/productController.js
+++ backened/controller/productController.js
@@ -1,5 +1,6 @@
 
 const Product = require("../models/productModel");
+const ErrorHander = require("../utils/errorhander");
 
 exports.createProduct = async(req,res,next)=>{
     const product = await Product.create(req.body);
@@ -18,18 +19,28 @@ exports.getAllProducts = async(req,res)=>{
     })
 }
 
+exports.getProductDetails = async(req,res,next)=>{
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return next(new ErrorHander("Product not found", 404));
+    }
+    res.status(200).json({
+       success:true,
+       product
+    })
+}
 
 exports.updateProduct = async (req,res,next)=>{
 
 let product = await Product.findById(req.params.id);
 
-if(!product){
-return res.status(500).json({
-    success:false,
-    message:"Product not found"
-})
+    if(!product){
+        return next(new ErrorHander("Product not found", 404));
+    }
 
-}
+
 
 product = await Product.findByIdAndUpdate(req.params.id,req.body,{
     new:true,
@@ -48,11 +59,7 @@ exports.deleteProduct = async (req,res,next)=>{
     let product = await Product.findById(req.params.id);
     
     if(!product){
-    return res.status(500).json({
-        success:false,
-        message:"Product not found"
-    })
-    
+        return next(new ErrorHander("Product not found", 404));
     }
 
     await product.remove();
---
+++ backened/middleware/error.js
@@ -0,0 +1,15 @@
+const ErrorHandler = require("../utils/errorhander");
+
+
+//internal server error
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
+
+  res.status(err.statusCode).json({
+    success: false,
+    message: err.message,
+  });
+}
+
+
