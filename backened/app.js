b620e35e56ebbb18691b17e8b013512e30660a0c
“jameela-khan” <jameelakhan52@gmail.com>
2023-01-13 15:40:22 +0530

    add user authentication

--- backened/app.js
+++ backened/app.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const app = express();
-// const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-// const fileUpload = require("express-fileupload");
+const fileUpload = require("express-fileupload");
 const path = require("path");
-
+const dotenv = require("dotenv");
 const errorMiddleware = require("./middleware/error");
 
 // Config
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "backend/config/config.env" });
-}
+dotenv.config( { path : 'backened/config/config.env'} );
 
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(fileUpload());
+app.use(fileUpload());
 
 // Route Imports
 const product = require("./routes/productRoute");
