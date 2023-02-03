12a0442a822965e2a9ed6348bb9568d505096e9a
“jameela-khan” <jameelakhan52@gmail.com>
2023-02-03 11:21:07 +0530

    Changes in index page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-// import {ToastContainer } from 'react-toastify';
-// import { positions, transitions, Provider as AlertProvider } from "react-toastify";
-// import 'react-toastify/dist/ReactToastify.css';
-// import AlertTemplate from "react-alert-template-basic";
 
-// const options = {
-//   timeout: 5000,
-//   position: positions.BOTTOM_CENTER,
-//   transition: transitions.SCALE,
-// };
+// ReactDOM.render(
+//   <Provider store={store}>
+   
+//       <App />
+   
+//   </Provider>,
+//   document.getElementById("root")
+// );
 
-ReactDOM.render(
-  <Provider store={store}>
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+    <Provider store={store}>
    
       <App />
    
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
